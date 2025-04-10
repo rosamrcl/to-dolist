c68f7160179e49457d47b590f34e1e24b6f8d069
Rosa <164552124+rosamrcl@users.noreply.github.com>
2025-04-10 17:14:12 -0300

    .

--- app/index.php
+++ app/index.php
@@ -1,4 +1,29 @@
 <?php
 $url="http://localhost/to-dolist/api/";
-curl_init($url);
+
+function GetTest($url){
+    $ambrosio=curl_init($url); 
+    curl_setopt($ambrosio, CURLOPT_RETURNTRANSFER,true);
+    $res=curl_exec($ambrosio);
+    curl_close($ambrosio);
+    echo "Resposta GET:\n";
+    echo $res . "\n\n";
+}
+function PostTest($url, $titulo){
+    $dados=json_encode(['titulo'=>$titulo]);
+
+    $ambrosio=curl_init($url);
+    curl_setopt($ambrosio, CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ambrosio, CURLOPT_POST,true);
+    curl_setopt($ambrosio, CURLOPT_HTTPHEADER,['Content-Type:application/json']);
+    curl_setopt($ambrosio, CURLOPT_POSTFIELDS,$dados);
+
+    $res=curl_exec($ambrosio);
+    curl_close($ambrosio);
+    echo "Resposta POST:\n";
+    echo $res . "\n\n";
+}
+GetTest($url);
+PostTest($url, 'Testar Curl via PHP');
+
 ?>
